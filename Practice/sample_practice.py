47fd2beb7f46f2e325ea8baf5744eade5c059e79
avinpandey <avinpandey@deloitte.com>
2022-03-10 19:31:35 +0530

    Added sas7bdat file extension

--- Practice/sample_practice.py
+++ Practice/sample_practice.py
@@ -1,10 +1,28 @@
-ls = [20, 30, 30, 20, 10]
-ls[:] = [(x / 10) for x in ls]
-for x in range(0, 5):
-    print(x)
+import logging
+
+from sas7bdat import SAS7BDAT
+import sys
+import pandas as pd
 
 
-for x in ls:
-    if x == 20 :
-        list(x).append(10)
-print(len(ls),ls)
+def write_sas7bdat(source_file_path, target_file_path):
+    source_file_path = source_file_path.strip()
+    target_file_path = target_file_path.strip()
+    pass
+
+
+def read_sas7bdat(source_file_path):
+    try:
+        source_file_path = source_file_path.strip()
+        with SAS7BDAT(source_file_path) as read_obj:
+            df = read_obj.to_data_frame()
+            print(df.head(1,truncate=False))
+    except:
+        print(logging.error('Error'))
+    pass
+
+
+source_path = r'C:\Users\avinpandey\Desktop\sas7bdat\DCSKINPRODUCT.sas7bdat'
+read_sas7bdat(source_path)
+target_path = r'C:\Users\avinpandey\Desktop\sas7bdat'
+write_sas7bdat(source_path,target_path)
