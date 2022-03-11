3fea8bc8a08283361a5e0957e1075c80e7f069cb
avinpandey <avinpandey@deloitte.com>
2022-03-11 10:56:25 +0530

    Added the context Manager file

--- Practice/ContextManager.py
+++ Practice/ContextManager.py
@@ -1,7 +1,20 @@
 class ContextManager:
 
-    def __init__(self):
-        pass
+    def __init__(self, name, mode):
+        print('Initialize')
+        self.name = name
+        self.mode = mode
+        self.file = None
 
-    def __exit__(self):
-        pass
+    def __enter__(self):
+        print('Enter')
+        self.file = open(self.name, self.mode)
+        return self.file
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        print('Exit')
+        self.file.close()
+
+
+with ContextManager(r'C:\Users\avinpandey\Desktop\Refund.txt', 'w') as manager:
+    manager.write('Test abhi baki hai mere dost')
